113119de3a85ba27b607988982223435f7d2368d
SofiAKOS <123480309+SofiAKOS@users.noreply.github.com>
2023-03-21 23:51:24 +0000

    Finish 4 homework

--- homeworkfourth.js
+++ homeworkfourth.js
@@ -35,3 +35,74 @@ function sumSliceArray(arr, first, second) {
       }
        
 //3
+function calcRectangleArea(width, height) {
+  if (typeof width !== 'number' || typeof height !== 'number') {
+    throw new Error('Both parameters must be numbers.');
+  }
+  return width * height;
+}
+
+try {
+  const width = 10;
+  const height = '20'; 
+  const area = calcRectangleArea(width, height);
+  console.log(`The area of the rectangle is ${area}.`);
+} catch (error) {
+  console.error(error.message);
+}
+
+//4
+class MonthException extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "MonthException";
+  }
+}
+
+function showMonthName(monthNum) {
+  const monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+  
+  if (monthNum < 1 || monthNum > 12) {
+    throw new MonthException("Incorrect month number.");
+  }
+  
+  return monthNames[monthNum - 1];
+}
+
+try {
+  console.log(showMonthName(5)); 
+  console.log(showMonthName(14)); 
+} catch (e) {
+  if (e instanceof MonthException) {
+    console.log(e.message); 
+  } else {
+    console.log("Unexpected error:", e);
+  }
+}
+//5
+function showUser(id) {
+  if (id < 0) {
+    throw new Error('ID must not be negative: ' + id);
+  }
+  return { id: id };
+}
+
+function showUsers(ids) {
+  var validIds = [];
+  for (var i = 0; i < ids.length; i++) {
+    try {
+      var user = showUser(ids[i]);
+      validIds.push(user);
+    } catch (error) {
+      console.log('Error:', error.message);
+    }
+  }
+  return validIds;
+}
+
+console.log(showUsers([7, -12, 44, 22])); 
+
+
